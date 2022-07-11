c87f826b8428f6456f3fe6d07fd29ea2e8c39b42
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-07-10 21:40:24 -0300

    primer commit

---
+++ source/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(process.env.PORT || 2020, console.log("Servidor corriendo en el puerto 2020"));
+
+const public = path.resolve(__dirname, './public')
+
+app.use(express.static(public));
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
+
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
