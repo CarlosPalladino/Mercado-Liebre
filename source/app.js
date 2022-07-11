b310e6da7c95bd90a3425e58ad8ca9e2316efbff
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-07-11 14:25:50 -0300

    cambios

--- source/app.js
+++ source/app.js
@@ -8,8 +8,8 @@ const public = path.resolve(__dirname, '../public')
 
 app.use(express.static(public));
 
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./source/views/home.html")));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
 
-app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './source/views/login.html')))
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
 
-app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './source/views/register.html')))
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
